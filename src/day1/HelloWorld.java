e1ea7feb7cbec91adb257eb2f6fbbb861b165147
Zgurskiy11 <zgurskiy11@gmail.com>
2021-05-21 09:19:42 -0600

    Initial commit

--- src/day1/HelloWorld.java
+++ src/day1/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
         System.out.println("Hello World");
         System.out.println("Adding");
         System.out.println();
+        System.out.println(" = ");
     }
 }
